9ef3d6e7c56d7456952eefaec0b70e091c6d14b1
Fritz Larco <flarco@live.com>
2022-07-24 16:23:34 -0300

    addd pointer functions

--- helpers.go
+++ helpers.go
@@ -628,3 +628,23 @@ func IsMatched(filters []string, name string) bool {
 	}
 	return false
 }
+
+// String returns a pointer to the string value passed in.
+func String(v string) *string {
+	return &v
+}
+
+// Int returns a pointer to the int value passed in.
+func Int(v int) *int {
+	return &v
+}
+
+// Int64 returns a pointer to the int64 value passed in.
+func Int64(v int64) *int64 {
+	return &v
+}
+
+// Bool returns a pointer to the bool value passed in.
+func Bool(v bool) *bool {
+	return &v
+}
