f4230a09dc491ddef6adfbf0bd513671427d192b
Fritz Larco <flarco@live.com>
2024-10-25 16:02:24 -0300

    add slack props

--- net/slack.go
+++ net/slack.go
@@ -25,7 +25,36 @@ type SlackMessage struct {
 	Channel     string            `json:"channel,omitempty"`
 	Type        string            `json:"type,omitempty"`
 	Text        string            `json:"text,omitempty"`
+	Color       string            `json:"color,omitempty"`
 	Attachments []SlackAttachment `json:"attachments,omitempty"`
+	Blocks      []SlackBlock      `json:"blocks,omitempty"`
+}
+
+// SlackBlock represents a Slack block
+type SlackBlock struct {
+	Type     string        `json:"type"`
+	Text     *SlackText    `json:"text,omitempty"`
+	Fields   []SlackField  `json:"fields,omitempty"`
+	Elements []interface{} `json:"elements,omitempty"`
+}
+
+// SlackText represents text within a Slack block
+type SlackText struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+// SlackField represents a field in a Slack section
+type SlackField struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short"`
+}
+
+// SlackSection is a helper struct for creating section blocks
+type SlackSection struct {
+	Text   string
+	Fields []*SlackField
 }
 
 // SlackAttachment is a Slack Attachement
@@ -53,6 +82,34 @@ type SlackAttachment struct {
 // NewSlackClient creates a new slack client
 func NewSlackClient(url string) *SlackClient {
 	return &SlackClient{WebHookURL: url}
+}
+
+// AddSection adds a new section block to the message
+func (m *SlackMessage) AddSection(section *SlackSection) {
+	block := SlackBlock{
+		Type: "section",
+	}
+
+	if section.Text != "" {
+		block.Text = &SlackText{
+			Type: "mrkdwn",
+			Text: section.Text,
+		}
+	}
+
+	if len(section.Fields) > 0 {
+		fields := make([]SlackField, len(section.Fields))
+		for i, f := range section.Fields {
+			fields[i] = SlackField{
+				Title: f.Title,
+				Value: f.Value,
+				Short: f.Short,
+			}
+		}
+		block.Fields = fields
+	}
+
+	m.Blocks = append(m.Blocks, block)
 }
 
 // Send sends a slack message
